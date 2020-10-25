84af8785431df2934e43a96bce806dc6e817d270
Arun Ravishankar <aravishankar2@gmail.com>
2020-10-25 12:33:59 -0500

    image component harcoded url, should be fixed

--- src/components/image.component.js
+++ src/components/image.component.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { API } from '../config'
 
 const Image = ({ product, url, theme }) => (
     <div className="product-img mt-1" style={theme ? { boxShadow: `0 10px 16px 0 ${theme.box_shadow}, 0 6px 20px 0 ${theme.box_shadow}` } : {}}>
--- src/components/install_shot.component.js
+++ src/components/install_shot.component.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { API } from '../config'
 
 const InstallShot = ({ product, url }) => (
     <div className="product-img mt-2">
-        <img src={`http://localhost:8000/api/${url}/installshot/${product._id}`} alt={product.name} className="mw-100 mh-100"/>
+        <img src={`${API}/${url}/installshot/${product._id}`} alt={product.name} className="mw-100 mh-100"/>
     </div>
 )
 
