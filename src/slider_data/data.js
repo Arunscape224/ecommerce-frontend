3b45b525fb20f7d5d601d39553b5b4f8623fd659
Arun Ravishankar <aravishankar2@gmail.com>
2020-10-27 09:12:37 -0500

    problem with slider, still working on fix

--- src/slider_data/data.js
+++ src/slider_data/data.js
@@ -57,36 +57,6 @@ export const data = {
         "shipping": true,
         "createdAt": '2020-06-25T18:52:03.072+00:00',
         "updatedAt": '2020-06-25T18:53:48.007+00:00'
-    },
-    
-    {
-        "_id": "5ef4f253636ec550baf03e91",
-        "index": 4,
-        "categories": [],
-        "photo": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg",
-        "soldPer": "pc",
-        "name": "Skyline",
-        "price": 34.2,
-        "quantity": 10000,
-        "howManySold": 0,
-        "shipping": true,
-        "createdAt": '2020-06-25T18:52:03.072+00:00',
-        "updatedAt": '2020-06-25T18:53:48.007+00:00'
-    },
-    
-    {
-        "_id": "5ef4f253636ec550baf03e91",
-        "index": 5,
-        "categories": [],
-        "photo": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg",
-        "soldPer": "pc",
-        "name": "Skyline",
-        "price": 34.2,
-        "quantity": 10000,
-        "howManySold": 0,
-        "shipping": true,
-        "createdAt": '2020-06-25T18:52:03.072+00:00',
-        "updatedAt": '2020-06-25T18:53:48.007+00:00'
     }
     ]
 }
